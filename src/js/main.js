5dfa39c00e6cabd5885db74521f8d23b238cd9ba
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-12-06 17:40:41 +0100

    Added vue.

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import App from './components/App.vue';
+import Home from './components/Home.vue';
+
+Vue.use(Router);
+
+const routes = [{
+  path: '/',
+  name: 'home',
+  component: Home
+}, {
+  path: '*',
+  redirect: {
+    name: 'home'
+  }
+}];
+
+const router = new Router({
+  mode: 'history',
+  routes: routes
+});
+
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
+});
+
+Object.assign(App, {
+  el: '#app',
+  router: router
+});
+
+const app = new Vue(App);
