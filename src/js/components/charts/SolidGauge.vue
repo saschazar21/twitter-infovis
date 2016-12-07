e7d9ab69a95493631252c1f955470a402ea947c5
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2016-12-07 22:11:18 +0100

    Added first chart components with some mock data and test code.

---
+++ src/js/components/charts/SolidGauge.vue
@@ -0,0 +1,122 @@
+<script>
+  import Highcharts from 'highcharts'
+  import SolidGaugeChart from 'highcharts/modules/solid-gauge'
+  SolidGaugeChart(Highcharts)
+
+  export default {
+    name: 'SolidGauge',
+    mounted() {
+      this.initChart()
+    },
+    methods: {
+      initChart() {
+        let gaugeOptions = this.getChartOptions()
+
+        const chart = Highcharts.chart('chart-container-speed', Highcharts.merge(gaugeOptions, {
+          yAxis: {
+            min: 0,
+            max: 200,
+            title: {
+              text: ''
+            }
+          },
+
+          credits: {
+            enabled: false
+          },
+
+          series: [{
+            name: 'Speed',
+            data: [
+              0
+            ],
+            dataLabels: {
+              format: `
+              <div style="text-align:center">
+                <span style="font-size:25px;color:
+                  ${((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black')}"
+                >
+                  {y}
+                </span>
+                <br>
+                <span style="font-size:12px;color:silver">Tweets/s</span>
+              </div>
+              `
+            },
+            tooltip: {
+              valueSuffix: ' Tweets/s'
+            }
+          }]
+
+        }))
+
+        setInterval(() => {
+          let point = chart.series[0].points[0];
+          let newVal = point.y + 20;
+          point.update(newVal);
+        }, 2000)
+      },
+      getChartOptions() {
+        return {
+
+          chart: {
+            type: 'solidgauge'
+          },
+
+          title: null,
+
+          pane: {
+            center: ['50%', '75%'],
+            size: '100%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+              backgroundColor: (
+                Highcharts.theme && Highcharts.theme.background2
+              ) || '#EEE',
+              innerRadius: '60%',
+              outerRadius: '100%',
+              shape: 'arc'
+            }
+          },
+
+          tooltip: {
+            enabled: false
+          },
+
+          // the value axis
+          yAxis: {
+            stops: [
+              [0.1, '#55BF3B'], // green
+              [0.5, '#DDDF0D'], // yellow
+              [0.9, '#DF5353'] // red
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 2,
+            title: {
+              y: -70
+            },
+            labels: {
+              y: 16
+            }
+          },
+
+          plotOptions: {
+            solidgauge: {
+              dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+              }
+            }
+          }
+        };
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="chart-container-speed"></div>
+</template>
---
+++ src/js/components/charts/StarPlot.vue
@@ -0,0 +1,84 @@
+<script>
+  import Highcharts from 'highcharts'
+  import SolidGaugeChart from 'highcharts/modules/solid-gauge'
+  SolidGaugeChart(Highcharts)
+
+  export default {
+    name: 'StarPlot',
+    mounted() {
+      this.initChart()
+    },
+    methods: {
+      initChart() {
+        const chart = Highcharts.chart('chart-container-star', {
+
+          chart: {
+            polar: true,
+            type: 'line'
+          },
+
+          credits: {
+            enabled: false
+          },
+
+          title: {
+            text: ''
+          },
+
+          pane: {
+            size: '100%',
+            center: ['50%', '60%'],
+          },
+
+          xAxis: {
+            categories: ['#tag1', '#tag2', '#tag3', '#tag4','#tag5'
+            ],
+            tickmarkPlacement: 'on',
+            lineWidth: 0
+          },
+
+          yAxis: {
+            gridLineInterpolation: 'polygon',
+            lineWidth: 0,
+            min: 0
+          },
+
+          tooltip: {
+            shared: true,
+            pointFormat: '<span><b>{point.y:,.0f}</b> tweets<br/>'
+          },
+
+          // legend: {
+          //   align: 'right',
+          //   verticalAlign: 'top',
+          //   y: 70,
+          //   layout: 'vertical'
+          // },
+
+          series: [{
+            name: 'Tweets',
+            type: 'area',
+            data: [43000, 19000, 20000, 35000, 17000],
+            pointPlacement: 'on'
+          }]
+
+        })
+
+        setInterval(() => {
+          let point = chart.series[0].points[0]
+          let point2 = chart.series[0].points[1]
+          let newVal = point.y + 10000
+          let newVal2 = point2.y + 5000
+          point.update(newVal)
+          setTimeout(() => {
+            point2.update(newVal2)
+          }, 500)
+        }, 2000)
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="chart-container-star"></div>
+</template>
